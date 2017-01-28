682e472ed02df1ae4881b9d66f0acb81fedce250
solodynamo <bittuf3@gmail.com>
2017-01-29 04:27:13 +0530

    Magic string name of events seperated from main file

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //Requiring the event module defined in events.js
 var Event = require('./events');
+//Requiring our magic Strings/ Predefined events
+var Config = require('./config');
 
 
 var emtr = new Event();
@@ -7,31 +9,31 @@ var emtr = new Event();
 //We can any custom event and can bind listner along with its callback function which will get invoked
 //when that event is emitted.
 
-emtr.on('Login', function() {
+emtr.on(Config.events.login, function() {
     /*
         DO YOUR OWN STUFF HERE WHEN A USER LOGS IN
     */
 });
 
-emtr.on('Logout', function() {
+emtr.on(Config.events.logout, function() {
     /*
       DO YOUR OWN STUFF HERE WHEN A USER JUST LOGS OUT
     */
 });
 
-emtr.on('Forgot', function() {
+emtr.on(Config.events.forgot, function() {
     /*
       DO YOUR OWN STUFF HERE WHEN A USER FORGOTS HIS/HER PASSOWRD/USERNAME ETC
     */
 })
 
-emtr.on('Update', function() {
+emtr.on(Config.events.update, function() {
     /*
       DO YOUR OWN STUFF HERE WHEN AN UPDATE EVENT OCCURS TO KEEP USER BUSY
     */
 })
 
-emtr.on('Expire', function() {
+emtr.on(Config.events.expire, function() {
     /*
       DO YOUR OWN STUFF HERE WHEN A USER'S TOKEN EXPIRE
     */
@@ -40,5 +42,5 @@ emtr.on('Expire', function() {
 /* For making the eventListner to perform stuff when a particular event happens we
 emit a particular event like below */
 
-emtr.emit('Expire');
-emtr.emit('Logout');
+emtr.emit('EXPIRE');//Will trigger the expire event listener callback
+emtr.emit('LOGOUT');//Will trigger the logout event listener callback
---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    events: {
+        login: "LOGIN",
+        logout: "LOGOUT",
+        forgot: "FORGOT",
+        update: "UPDATE",
+        expire: "EXPIRE"
+    }
+}
