703a893c543e649090c48be1b107fad7145c08b7
solodynamo <bittuf3@gmail.com>
2017-01-28 22:32:02 +0530

    Identation and minor pattern change

--- app.js
+++ app.js
@@ -8,31 +8,31 @@ var emtr = new Event();
 //when that event is emitted.
 
 emtr.on('Login', function() {
-  /*
-      DO YOUR OWN STUFF HERE WHEN A USER LOGS IN
-  */
+    /*
+        DO YOUR OWN STUFF HERE WHEN A USER LOGS IN
+    */
 });
 
 emtr.on('Logout', function() {
-  /*
-    DO YOUR OWN STUFF HERE WHEN A USER JUST LOGS OUT
-  */
+    /*
+      DO YOUR OWN STUFF HERE WHEN A USER JUST LOGS OUT
+    */
 });
 
 emtr.on('Forgot', function() {
-  /*
-    DO YOUR OWN STUFF HERE WHEN A USER FORGOTS HIS/HER PASSOWRD/USERNAME ETC
-  */
+    /*
+      DO YOUR OWN STUFF HERE WHEN A USER FORGOTS HIS/HER PASSOWRD/USERNAME ETC
+    */
 })
 
 emtr.on('Update', function() {
-  /*
-    DO YOUR OWN STUFF HERE WHEN AN UPDATE EVENT OCCURS TO KEEP USER BUSY
-  */
+    /*
+      DO YOUR OWN STUFF HERE WHEN AN UPDATE EVENT OCCURS TO KEEP USER BUSY
+    */
 })
 
 emtr.on('Expire', function() {
-  /*
-    DO YOUR OWN STUFF HERE WHEN A USER'S TOKEN EXPIRE
-  */
+    /*
+      DO YOUR OWN STUFF HERE WHEN A USER'S TOKEN EXPIRE
+    */
 })
